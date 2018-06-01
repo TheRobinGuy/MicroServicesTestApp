d6a3e886a359d11129e9282a049147010720da68
ThatRobinGuy <gilmartin.glenn@gmail.com>
2018-06-01 21:42:19 +0100

    Builder Method Started

--- src/main/java/IonicApps/TestService/Entities/User.java
+++ src/main/java/IonicApps/TestService/Entities/User.java
@@ -1,9 +1,47 @@
 package IonicApps.TestService.Entities;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
+@Named
 @Table(name="User")
 public class User {
+    private String UserName;
+    private String Password;
+
+    @Inject
+    public User(UserBuilder userBuilder) {
+        User user = new User.UserBuilder()
+                .withUserName(userBuilder.UserName)
+                .withPassword(userBuilder.Password)
+                .build();
+    }
+
+    @Inject
+    public User(String userName, String password) {
+        this.UserName = userName;
+        this.Password = password;
+    }
+
+    public class UserBuilder{
+        private String UserName;
+        private String Password;
+
+        public UserBuilder withUserName(String userName){
+            this.UserName = userName;
+            return this;
+        }
+
+        public UserBuilder withPassword(String password){
+            this.Password = password;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
 }
